fb9b7097b1cd3b51e8f67fcd1bf132e78fe1ca9c
oubaoyao <673411498@qq.com>
2019-08-19 18:08:46 +0800

    第一次提交Unity2018.4.1f1

---
+++ Assets/Resources/TexturePainter ( Place Me Out of Resources)/Scripts/Save.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour
+{
+    public GameObject Model;
+    [SerializeField]
+    public Material[] OldModelMaterial;
+    [SerializeField]
+    private Material[] NewModelMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //OldModelMaterial = Model.GetComponent<MeshRenderer>().materials;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            NewModelMaterial = Model.GetComponent<MeshRenderer>().materials;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Model.GetComponent<MeshRenderer>().materials= OldModelMaterial;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Model.GetComponent<MeshRenderer>().materials= NewModelMaterial;
+        }
+
+    }
+}
