a4ada334a90e5c721abc2eb3cb25fb6394488031
oubaoyao <673411498@qq.com>
2019-08-20 09:36:44 +0800

    提交一次返回上一次

--- Assets/Resources/TexturePainter ( Place Me Out of Resources)/Scripts/Save.cs
+++ Assets/Resources/TexturePainter ( Place Me Out of Resources)/Scripts/Save.cs
@@ -6,33 +6,34 @@ public class Save : MonoBehaviour
 {
     public GameObject Model;
     [SerializeField]
-    public Material[] OldModelMaterial;
+    public Material OldModelMaterial;
     [SerializeField]
-    private Material[] NewModelMaterial;
+    private Material NewModelMaterial;
+    public RenderTexture canvasTexture;
 
     // Start is called before the first frame update
     void Start()
     {
-        //OldModelMaterial = Model.GetComponent<MeshRenderer>().materials;
+        OldModelMaterial = Model.GetComponent<MeshRenderer>().materials[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            NewModelMaterial = Model.GetComponent<MeshRenderer>().materials;
-        }
+        //if (Input.GetKeyDown(KeyCode.J))
+        //{
+        //    OldModelMaterial.mainTexture = new Texture2D(canvasTexture.width, canvasTexture.height, TextureFormat.RGB24, false);
+        //}
 
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-            Model.GetComponent<MeshRenderer>().materials= OldModelMaterial;
-        }
+        //if(Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    Model.GetComponent<MeshRenderer>().materials= OldModelMaterial;
+        //}
 
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            Model.GetComponent<MeshRenderer>().materials= NewModelMaterial;
-        }
+        //if(Input.GetKeyDown(KeyCode.E))
+        //{
+        //    Model.GetComponent<MeshRenderer>().materials= NewModelMaterial;
+        //}
 
     }
 }
